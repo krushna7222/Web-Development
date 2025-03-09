220cafa75bc9e6e1be7a5dce8edcec2de3742ea5
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-03-09 23:55:44 +0530

    Heading Component

---
+++ React_JS/ToDo-App/src/component/ToDoHeading.jsx
@@ -0,0 +1,11 @@
+function ToDoHeading(){
+    return (
+        <div id="todoapp">
+            <h1 align="center">Todo App</h1>
+            
+
+        </div>
+    )
+
+}
+export default ToDoHeading
