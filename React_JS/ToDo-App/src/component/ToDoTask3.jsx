7bf7141c90e46416af2453c7c548c88839ce8852
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-03-12 20:47:59 +0530

    Create ToDoTask3.jsx

---
+++ React_JS/ToDo-App/src/component/ToDoTask3.jsx
@@ -0,0 +1,12 @@
+function ToDoTask3(){
+    return (
+        <div id="todoapp">
+               <div className="row rows">
+                    <div className="col-5">Go To College</div>
+                    <div className="col-4">04/10/2023</div>
+                    <div className="col-3"><button class="btn btn-danger">Delete</button></div>
+                </div>
+        </div>
+    )
+}
+export default ToDoTask3
