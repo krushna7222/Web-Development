531fe542a08582606766d61cbd80bee6998a63f4
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-03-12 20:47:49 +0530

    Create ToDoTask1.jsx

---
+++ React_JS/ToDo-App/src/component/ToDoTask1.jsx
@@ -0,0 +1,14 @@
+function ToDoTask1(){
+    return (
+        <div id="todoapp">
+                <div className="row rows">
+                    <div className="col-5"><input type="text" placeholder="Enter the "/></div>
+                    <div className="col-4"><input type="date" /></div>
+                    <div className="col-3"><button class="btn btn-success">Add</button></div>
+                </div>
+
+            </div>
+    )
+
+}
+export default ToDoTask1
